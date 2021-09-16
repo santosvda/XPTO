b906e9c7f83c35dee19f1c4b112679bed25cfcf5
santosvda <felipesantosacct@gmail.com>
2021-09-15 22:19:09 -0300

    Ajuste Tabela - adicionando filtros e opcoes

--- XPTO-App/src/pages/Product.vue
+++ XPTO-App/src/pages/Product.vue
@@ -3,139 +3,120 @@
     <q-table
       :data="data"
       :columns="columns"
-      row-key="name"
-    />
+      row-key="title"
+      :filter="filter"
+      :separator="separator"
+      no-data-label="I didn't find anything for you"
+      no-results-label="The filter didn't uncover any results"
+    >
+    <template v-slot:header="props">
+        <q-tr :props="props">
+          <q-th auto-width />
+          <q-th
+            v-for="col in props.cols"
+            :key="col.name"
+            :props="props"
+          >
+            {{ col.label }}
+          </q-th>
+        </q-tr>
+      </template>
+
+      <template v-slot:body="props">
+        <q-tr :props="props">
+          <q-td auto-width>
+            <q-btn size="sm" color="accent" round dense @click="props.expand = !props.expand" :icon="props.expand ? 'remove' : 'add'" />
+          </q-td>
+          <q-td
+            v-for="col in props.cols"
+            :key="col.name"
+            :props="props"
+          >
+            {{ col.value }}
+          </q-td>
+        </q-tr>
+        <q-tr v-show="props.expand" :props="props">
+          <q-td colspan="100%" class="justify-evenly">
+            <q-list padding class="menu-list">
+            <q-item clickable v-ripple  @click="openModal(props.row)" >
+              <q-item-section avatar>
+                <q-btn color="yellow" round dense icon="eva-edit-2-outline" />
+              </q-item-section>
+
+              <q-item-section>
+                Editar
+              </q-item-section>
+            </q-item>
+            <q-item clickable v-ripple @click="openModal(props.row)">
+              <q-item-section avatar>
+                <q-btn color="red" round dense icon="eva-trash-2-outline" />
+              </q-item-section>
+
+              <q-item-section>
+                Remover
+              </q-item-section>
+            </q-item>
+            </q-list>
+          </q-td>
+        </q-tr>
+      </template>
+    <template v-slot:top-right>
+        <q-input borderless dense debounce="300" v-model="filter" placeholder="Search">
+          <template v-slot:append>
+            <q-icon name="search" />
+          </template>
+        </q-input>
+      </template>
+    </q-table>
   </div>
 </template>
+
 
 <script>
 export default {
   name: 'Product',
   data () {
     return {
-            columns: [
-            {
-            name: 'product',
-            required: true,
-            label: 'Produto',
-            align: 'left',
-            field: row => row.name,
-            format: val => `${val}`,
-            sortable: true,
-            classes: 'bg-grey-2 ellipsis',
-            style: 'max-width: 100px',
-            headerClasses: 'bg-primary text-white'
-            },
-            { name: 'price', align: 'center', label: 'Preço', field: 'price', sortable: true },
-            { name: 'fat', label: 'Fat (g)', field: 'fat', sortable: true },
-            { name: 'carbs', label: 'Carbs (g)', field: 'carbs' },
-            { name: 'protein', label: 'Protein (g)', field: 'protein' },
-            { name: 'sodium', label: 'Sodium (mg)', field: 'sodium' },
-            { name: 'calcium', label: 'Calcium (%)', field: 'calcium', sortable: true, sort: (a, b) => parseInt(a, 10) - parseInt(b, 10) },
-            { name: 'iron', label: 'Iron (%)', field: 'iron', sortable: true, sort: (a, b) => parseInt(a, 10) - parseInt(b, 10) }
+        filter: '',
+        separator: 'cell',
+        columns: [
+        {
+        name: 'title',
+        required: true,
+        label: 'Produto',
+        align: 'left',
+        field: row => row.title,
+        format: val => `${val}`,
+        sortable: true,
+        classes: 'bg-grey-2 ellipsis',
+        style: 'max-width: 100px',
+        headerClasses: 'bg-primary text-white'
+        },
+        { name: 'price', align: 'center', label: 'Preço', field: 'price', sortable: true},
+        { name: 'barcode', label: 'Código de Barras', field: 'barCode', sortable: true },
         ],
         data: [
             {
-            name: 'Frozen Yogurt',
-            calories: 159,
-            fat: 6.0,
-            carbs: 24,
-            protein: 4.0,
-            sodium: 87,
-            calcium: '14%',
-            iron: '1%'
+            title: 'Notebook ASUS Aspire-5',
+            price: 5000,
+            barCode: 3123123123123
             },
             {
-            name: 'Ice cream sandwich',
-            calories: 237,
-            fat: 9.0,
-            carbs: 37,
-            protein: 4.3,
-            sodium: 129,
-            calcium: '8%',
-            iron: '1%'
+            title: 'Cadeira MX-5',
+            price: 800,
+            barCode: 654654654654654
             },
             {
-            name: 'Eclair',
-            calories: 262,
-            fat: 16.0,
-            carbs: 23,
-            protein: 6.0,
-            sodium: 337,
-            calcium: '6%',
-            iron: '7%'
-            },
-            {
-            name: 'Cupcake',
-            calories: 305,
-            fat: 3.7,
-            carbs: 67,
-            protein: 4.3,
-            sodium: 413,
-            calcium: '3%',
-            iron: '8%'
-            },
-            {
-            name: 'Gingerbread',
-            calories: 356,
-            fat: 16.0,
-            carbs: 49,
-            protein: 3.9,
-            sodium: 327,
-            calcium: '7%',
-            iron: '16%'
-            },
-            {
-            name: 'Jelly bean',
-            calories: 375,
-            fat: 0.0,
-            carbs: 94,
-            protein: 0.0,
-            sodium: 50,
-            calcium: '0%',
-            iron: '0%'
-            },
-            {
-            name: 'Lollipop',
-            calories: 392,
-            fat: 0.2,
-            carbs: 98,
-            protein: 0,
-            sodium: 38,
-            calcium: '0%',
-            iron: '2%'
-            },
-            {
-            name: 'Honeycomb',
-            calories: 408,
-            fat: 3.2,
-            carbs: 87,
-            protein: 6.5,
-            sodium: 562,
-            calcium: '0%',
-            iron: '45%'
-            },
-            {
-            name: 'Donut',
-            calories: 452,
-            fat: 25.0,
-            carbs: 51,
-            protein: 4.9,
-            sodium: 326,
-            calcium: '2%',
-            iron: '22%'
-            },
-            {
-            name: 'KitKat',
-            calories: 518,
-            fat: 26.0,
-            carbs: 65,
-            protein: 7,
-            sodium: 54,
-            calcium: '12%',
-            iron: '6%'
+            title: 'Memória RAM 8GB 3000mhz',
+            price: 350.32,
+            barCode: 654654654654654654654654654654
             }
         ]
+    }
+    },
+    methods:{
+        openModal(data){
+            console.log(data)
         }
     }
 }
